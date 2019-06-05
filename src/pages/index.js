2a87b9672523e176d28d7f9bb264f41b945d3038
Alex Mireles <anchises1968@gmail.com>
2019-06-05 14:30:40 -0700

    added logo, appointment button

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,6 +13,15 @@ import { faLinkedin, faFacebook } from '@fortawesome/free-brands-svg-icons'
 
 import styled from "styled-components"
 
+const Logo = styled.figure`
+  max-width: 100px;
+  height: auto;
+  margin-left: auto;
+  margin-right: auto;
+  position: relative;
+  
+`
+
 const Title = styled.h1`
   font-size: 5rem;
   @media(max-width: 700px) {
@@ -28,7 +37,7 @@ const Profile = styled.figure`
   margin-left: auto;
   margin-right: auto;
   @media(max-width: 700px) {
-    margin-Top: 25px;
+    margin-top: 0px;
   }
 `
 
@@ -75,6 +84,9 @@ class BlogIndex extends React.Component {
               <h2 className="subtitle has-text-light is-uppercase">
                 {subTitle}
               </h2>
+              <Logo>
+                <Img fluid={data.logo.childImageSharp.fluid} />
+              </Logo>
             </div>
           </div>
         </section>
@@ -94,6 +106,21 @@ class BlogIndex extends React.Component {
                   <Quotes>
                   <TestimonialBubble/>
                   </Quotes>
+                  <br/>
+                  <br/>
+                  <div className="content">
+                    <div className="columns is-centered is-mobile" >
+                      <div className="column is-1-mobile" >
+                        <a href="mailto:https://heatherwsonora@gmail.com" aria-label="e-mail"><FontAwesomeIcon icon={faEnvelope} className="fa-2x mr-3 has-text-grey" /></a>
+                      </div>
+                      <div className="column is-1-mobile">
+                        <a href="https://www.linkedin.com/in/heather-watson-bab051180/" aria-label="Linkedin"><FontAwesomeIcon icon={faLinkedin} className="fa-2x mr-3 has-text-grey"/></a>
+                      </div>
+                      <div className="column is-1-mobile"> 
+                        <a href="https://www.facebook.com/golivewell/" aria-label="Facebook"><FontAwesomeIcon icon={faFacebook} className="fa-2x has-text-grey"/></a>
+                      </div>
+                    </div>
+                  </div>
               </div>
             </div>
            </div>
@@ -126,7 +153,30 @@ class BlogIndex extends React.Component {
         <section className="container">
           <Services/>
           <br/>
+          <br/>
+          <div className="content has-text-centered">
+            <a target="_top" style={{
+              backgroundColor: "#371797",
+              color: "white",
+              height: "40px",
+              textTransform: "uppercase",
+              fontFamily: "'Square Market', 'helvetica neue', helvetica, arial, sans-serif",
+              letterSpacing: "1px",
+              lineHeight: "38px",
+              padding: "0 28px",
+              borderRadius: "3px",
+              fontWeight: "500",
+              fontSize: "14px",
+              cursor: "pointer",
+              display: "inline-block",
+              boxShadow: "2px 2px 5px #888888"
+              }}
+              href="https://squareup.com/appointments/book/FTJHJJCD1JZEZ/heather-watson-conscious-wellness-sonora-ca" rel="nofollow">Book an Appointment
+            </a>
+
+          </div>
          
+          <br/>
         </section>
         <br/>
         <section className="section">
@@ -134,7 +184,7 @@ class BlogIndex extends React.Component {
           <div className="columns">
             <div className="column is-12">
               <div className="content has-text-centered">
-                <h1 className="title has-text-grey">Latest News</h1>
+                <h1 className="title has-text-grey">Heather's Blog</h1>
                 <hr/>
               </div>
             </div>
@@ -180,6 +230,7 @@ class BlogIndex extends React.Component {
                 <div className="content">
                   <h4>About Heather</h4>
                   <p className="has-text-justified">I am passionate about helping people improve their health. It is a privilege to work with each client. Each session is unique and different, based on what my client needs at the moment. I open my home office to you, which is set in a peaceful organic apple orchard in the middle of a forest near a running brook. The relaxing environment will aid in your healing experience.  Distance sessions are also available. My clients, who reside all over the United States, have found them to be very effective. Results do depend on the person, since, in many ways, they are the ones who are allowing and doing their own healing.  Feel free to contact me with any questions or concerns you might have. If you are reading this, there is a reason, and if you are ready to heal, please give me a call. I look forward to working with you!</p>
+                  <Link to="/about" className="button">Read more</Link>
                 </div>
               </div>
             </div>
@@ -206,21 +257,6 @@ class BlogIndex extends React.Component {
               <p className="has-text-light">or</p>
               <br/>
               <div className="button is-primary is-outlined">Book an Appointment</div>
-              
-              <div className="content">
-              <br/>
-                <div className="columns is-centered" >
-                  <div className="column is-1" >
-                    <a href="mailto:https://heatherwsonora@gmail.com" aria-label="e-mail"><FontAwesomeIcon icon={faEnvelope} className="fa-2x mr-3 has-text-light" /></a>
-                  </div>
-                  <div className="column is-1">
-                  <a href="https://www.linkedin.com/in/heather-watson-bab051180/" aria-label="Linkedin"><FontAwesomeIcon icon={faLinkedin} className="fa-2x mr-3 has-text-light"/></a>
-                  </div>
-                  <div className="column is-1"> 
-                    <a href="https://www.facebook.com/golivewell/" aria-label="Facebook"><FontAwesomeIcon icon={faFacebook} className="fa-2x has-text-light"/></a>
-                  </div>
-                </div>
-              </div>
             </div>
           </div>
         </section>
@@ -242,6 +278,13 @@ export const pageQuery = graphql`
     hero: file(absolutePath: { regex: "/joseph-barrientos-22900-unsplash.jpg/" }) {
       childImageSharp {
         fluid(maxWidth: 2048, quality: 100) {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+    logo: file(absolutePath: { regex: "/go-live-well-white.png/"}) {
+      childImageSharp {
+        fluid(maxWidth: 600) {
           ...GatsbyImageSharpFluid
         }
       }
